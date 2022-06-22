5e2b26f90a36bbd7d108d2b8ff86f22ba0527789
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-06-22 12:03:30 +0800

    chore(): :wrench: 配置修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'food-billboard', // Usually your GitHub org/user name.
-  projectName: '数据可视化大屏文档', // Usually your repo name.
+  projectName: 'create-chart-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
